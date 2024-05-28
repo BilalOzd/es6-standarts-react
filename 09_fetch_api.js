68de944345905aef495d0b11710e95194f428a4d
Bilal Ozdemir <ozdemirbilal01@gmail.com>
2024-05-28 22:54:23 +0300

    fetch API details

--- 09_fetch_api.js
+++ 09_fetch_api.js
@@ -20,24 +20,32 @@ const sendDataAsync = async () => {
 const loadTodos = () => {
 	fetch('https://jsonplaceholder.typicode.com/todos', { method: 'GET' })
 		.then((response) => {
+            console.log('NORMAL FETCH - RESPONSE')
 			console.log('response', response);
 			return response.json();
 		})
 		.then((data) => {
+            console.log('NORMAL FETCH - DATA')
 			console.log('data', data);
 			return data;
 		})
 		.catch((err) => {
+            console.log('NORMAL FETCH - ERROR')
 			console.log('err', err);
 		});
 };
 
 const loadTodosAsync = async () => {
 	try {
+        console.log('ASYNC FETCH - START')
 		const response = await fetch('https://jsonplaceholder.typicode.com/todos');
 		const data = await response.json();
+        console.log('ASYNC FETCH - DATA')
 		console.log('data', data);
 	} catch (error) {
 		console.log('error', error);
 	}
 };
+
+console.log(loadTodos())
+console.log(loadTodosAsync())
